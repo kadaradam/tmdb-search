3d9758ba31d82dba672a9b4e241d4635465d7caf
kadaradam <xadam1996@gmail.com>
2023-01-24 16:16:42 +0100

    feat: navigate to movie details

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -4,6 +4,7 @@ import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/system';
 import Image from 'next/image';
+import NextLink from 'next/link';
 import { useState } from 'react';
 import { TmdbConfigType, TrendingType } from '../types';
 
@@ -18,54 +19,59 @@ const SearchItem = ({ item, configuration }: SearchItemProps) => {
 	);
 
 	return (
-		<AnimatedBox>
-			<Box
-				position="relative"
-				sx={{
-					width: '100%',
-					height: '200px',
-				}}
-			>
-				<Image
-					src={imgSrc}
-					alt={item.title}
-					fill
-					style={{
-						objectFit: 'contain',
-						margin: 'auto',
-						overflow: 'hidden',
+		<NextLink
+			href={`/movie/${item.id}`}
+			style={{ textDecoration: 'none', color: 'unset' }}
+		>
+			<AnimatedBox>
+				<Box
+					position="relative"
+					sx={{
+						width: '100%',
+						height: '200px',
 					}}
-					sizes="(max-width: 768px) 100vw,
-					(max-width: 1200px) 50vw,
-					33vw"
-					onError={() =>
-						setImgSrc('/assets/default-fallback-image.png')
-					}
-				/>
-			</Box>
-			<Typography variant="h6" fontWeight="bold">
-				{item.original_name || item.title}
-			</Typography>
-			<Typography variant="caption" textTransform="uppercase">
-				{item.vote_count} votes
-			</Typography>
-			{item.vote_count > 0 ? (
-				<Box>
-					<Rating
-						value={item.vote_average / 2}
-						readOnly
-						precision={0.5}
-						size="small"
-						emptyIcon={
-							<StarIcon
-								fontSize="inherit"
-								sx={{ fill: 'white', opacity: 0.25 }}
-							/>
+				>
+					<Image
+						src={imgSrc}
+						alt={item.title}
+						fill
+						style={{
+							objectFit: 'contain',
+							margin: 'auto',
+							overflow: 'hidden',
+						}}
+						sizes="(max-width: 768px) 100vw,
+								(max-width: 1200px) 50vw,
+								33vw"
+						onError={() =>
+							setImgSrc('/assets/default-fallback-image.png')
 						}
 					/>
 				</Box>
-			) : null}
-		</AnimatedBox>
+				<Typography variant="h6" fontWeight="bold">
+					{item.original_name || item.title}
+				</Typography>
+				<Typography variant="caption" textTransform="uppercase">
+					{item.vote_count} votes
+				</Typography>
+				{item.vote_count > 0 ? (
+					<Box>
+						<Rating
+							value={item.vote_average / 2}
+							readOnly
+							precision={0.5}
+							size="small"
+							emptyIcon={
+								<StarIcon
+									fontSize="inherit"
+									sx={{ fill: 'white', opacity: 0.25 }}
+								/>
+							}
+						/>
+					</Box>
+				) : null}
+			</AnimatedBox>
+		</NextLink>
 	);
 };
 
@@ -74,6 +80,11 @@ const AnimatedBox = styled(Box)({
 		transition: 'transform 450ms',
 		transform: 'translateY(-10px)',
 	},
+});
+
+const ATag = styled('a')({
+	textDecoration: 'none',
+	color: 'unset',
 });
 
 export default SearchItem;
